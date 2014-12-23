e174d539d6123d16d9eeed286d4a614c39415167
Liliya Gateva <liliya.gateva@gmail.com>
2014-12-22 21:39:23 -0500

    Second week classwork

---
+++ Classwork/xib/xib demo/xib demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xib demo
+//
+//  Created by Liliya Gateva on 12/22/14.
+//  Copyright (c) 2014 Liliya Gateva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
